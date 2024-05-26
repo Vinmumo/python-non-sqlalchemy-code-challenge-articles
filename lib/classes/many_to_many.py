66b6481db4fc47e31aeaab195266578d9f8ffbb8
vincent <vinnymummo@gmail.com>
2024-05-26 22:55:20 +0300

    commit

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -6,7 +6,22 @@ class Article:
         
 class Author:
     def __init__(self, name):
-        self.name = name
+        if not isinstance(name, str):
+            raise TypeError("Name must be of type str")
+        if len(name) == 0:
+            raise ValueError("Name must be longer than 0 characters")
+
+       
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if hasattr(self, '_name'):
+            raise AttributeError("Can't change the name after the author is instantiated")
 
     def articles(self):
         pass
